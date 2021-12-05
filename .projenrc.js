097fc1f1ebfcdc9e757a3464b53f1c82dd867e76
Elad Ben-Israel <benisrae@amazon.com>
2021-12-05 14:44:02 +0200

    chore: upgrade deps
    Signed-off-by: Elad Ben-Israel <benisrae@amazon.com>

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,6 @@
 const child = require('child_process');
 const path = require('path');
-const { JsiiProject } = require('projen');
+const { cdk } = require('projen');
 
 const DEFAULT_K8S_VERSION = '22';
 const SPEC_VERSION = k8sVersion();
@@ -17,7 +17,7 @@ function k8sVersion() {
   return match[1];
 }
 
-const project = new JsiiProject({
+const project = new cdk.JsiiProject({
   name: `cdk8s-plus-${SPEC_VERSION}`,
   description: `cdk8s+ is a software development framework that provides high level abstractions for authoring Kubernetes applications. cdk8s-plus-${SPEC_VERSION} synthesizes Kubernetes manifests for Kubernetes ${K8S_VERSION}`,
 
