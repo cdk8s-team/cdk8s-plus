e41aaa6b840c33544ebeb3210f3c4e5363bc4780
Eli Polonsky <epolon@amazon.com>
2022-04-13 18:07:01 +0300

    feat(daemon-set): introduce L2 (#504) (#577)
    Resolves https://github.com/cdk8s-team/cdk8s-plus/issues/375
    
    Note that I intentionally only added support for pod spec and `minReadySeconds` to get something out quickly.
    Other properties will be added at a later time.
    
    (cherry picked from commit fbc9f0ad4d8318a8d11d47186f7f55c69e0466ba)
    Signed-off-by: Eli Polonsky <epolon@amazon.com>
    
    # Conflicts:
    #	docs/java.md
    #	docs/python.md
    #	docs/typescript.md

---
+++ test/daemon-set.test.ts
@@ -0,0 +1,97 @@
+import { ApiObject, Testing } from 'cdk8s';
+import { Node } from 'constructs';
+import * as kplus from '../src';
+
+test('default child', () => {
+
+  const chart = Testing.chart();
+  const ds = new kplus.DaemonSet(chart, 'DaemonSet');
+  const defaultChild = Node.of(ds).defaultChild as ApiObject;
+
+  expect(defaultChild.kind).toEqual('DaemonSet');
+
+});
+
+test('defaults', () => {
+
+  const chart = Testing.chart();
+  new kplus.DaemonSet(chart, 'DaemonSet', {
+    containers: [{ image: 'image' }],
+  });
+
+  expect(Testing.synth(chart)).toMatchSnapshot();
+
+});
+
+test('custom', () => {
+
+  const chart = Testing.chart();
+  new kplus.DaemonSet(chart, 'DaemonSet', {
+    containers: [{ image: 'image' }],
+    minReadySeconds: 5,
+  });
+
+  expect(Testing.synth(chart)).toMatchSnapshot();
+
+});
+
+test('a label selector is automatically allocated', () => {
+
+  const chart = Testing.chart();
+
+  const ds = new kplus.DaemonSet(chart, 'DaemonSet');
+  ds.addContainer({ image: 'foobar' });
+
+  const expectedValue = 'test-DaemonSet-c8f77186';
+  const expectedSelector = { 'cdk8s.daemon-set': expectedValue };
+
+  // assert the k8s spec has it.
+  const spec = Testing.synth(chart)[0].spec;
+  expect(spec.selector.matchLabels).toEqual(expectedSelector);
+  expect(spec.template.metadata?.labels).toEqual(expectedSelector);
+
+  // assert the deployment object has it.
+  expect(ds.labelSelector).toEqual(expectedSelector);
+
+});
+
+test('no selector is generated if "defaultSelector" is false', () => {
+
+  const chart = Testing.chart();
+
+  const ds = new kplus.DaemonSet(chart, 'DaemonSet', {
+    defaultSelector: false,
+    containers: [{ image: 'foobar' }],
+  });
+
+  // assert the k8s spec doesnt have it.
+  const spec = Testing.synth(chart)[0].spec;
+  expect(spec.selector.matchLabels).toEqual({});
+  expect(spec.template.metadata?.labels).toEqual(undefined);
+
+  // assert the deployment object doesnt have it.
+  expect(ds.labelSelector).toEqual({});
+
+});
+
+test('can select by label', () => {
+
+  const chart = Testing.chart();
+
+  const ds = new kplus.DaemonSet(chart, 'DaemonSet', {
+    containers: [{ image: 'image' }],
+    defaultSelector: false,
+  });
+
+  const expectedSelector = { foo: 'bar' };
+
+  ds.selectByLabel('foo', expectedSelector.foo);
+
+  // assert the k8s spec has it.
+  const spec = Testing.synth(chart)[0].spec;
+  expect(spec.selector.matchLabels).toEqual(expectedSelector);
+
+  // assert the deployment object has it.
+  expect(ds.labelSelector).toEqual(expectedSelector);
+
+});
