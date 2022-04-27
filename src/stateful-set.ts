a3d88acbedb66fa81f8b5597ead3e6596604284b
Eli Polonsky <epolon@amazon.com>
2022-04-28 00:19:41 +0200

    feat(workload): expression selectors and pod metadata integration (#660)
    Introduce a new API for workload selectors that supports both `matchExpressions` as well as applying the entries in `matchLabels` to the pod metadata for automatic selection.
    
    BREAKING CHANGE: `defaultSelector` renamed to `select`. `selectByLabel` renamed to `select` and underwent an API change: `deployment.selectByLabel('key', 'value') -> deployment.select(LabelSelector.equals('key', 'value'))`
    
    Fixes https://github.com/cdk8s-team/cdk8s-plus/issues/414
    Fixes https://github.com/cdk8s-team/cdk8s-plus/issues/413

--- src/stateful-set.ts
+++ src/stateful-set.ts
@@ -134,7 +134,7 @@ export class StatefulSet extends workload.Workload {
     this.podManagementPolicy = props.podManagementPolicy ?? PodManagementPolicy.ORDERED_READY;
     this.minReady = props.minReady ?? Duration.seconds(0);
 
-    const selectors = Object.entries(this.labelSelector);
+    const selectors = Object.entries(this.matchLabels);
     for (const [k, v] of selectors) {
       this._service.addSelector(k, v);
     }
@@ -153,7 +153,8 @@ export class StatefulSet extends workload.Workload {
         spec: this._toPodSpec(),
       },
       selector: {
-        matchLabels: this.labelSelector,
+        matchExpressions: this.matchExpressions,
+        matchLabels: this.matchLabels,
       },
       podManagementPolicy: this.podManagementPolicy,
       updateStrategy: this.strategy._toKube(),
--- src/workload.ts
+++ src/workload.ts
@@ -13,12 +13,63 @@ export interface WorkloadProps extends AbstractPodProps {
   readonly podMetadata?: ApiObjectMetadata;
 
   /**
-   * Automatically allocates a pod selector for this workload.
+   * Automatically allocates a pod label selector for this workload and add
+   * it to the pod metadata. This ensures this workload manages pods created by
+   * its pod template.
    *
    * @default true
    */
-  readonly defaultSelector?: boolean;
+  readonly select?: boolean;
 
+}
+
+/**
+ * Possible operators.
+ */
+export enum LabelSelectorRequirementOperator {
+
+  /**
+   * In.
+   */
+  IN = 'In',
+
+  /**
+   * NotIn.
+   */
+  NOT_IN = 'NotIn',
+
+  /**
+   * Exists.
+   */
+  EXISTS = 'Exists',
+
+  /**
+   * DoesNotExist.
+   */
+  DOES_NOT_EXIST = 'DoesNotExist'
+}
+
+/**
+ * A label selector requirement is a selector that contains values, a key, and an operator that
+ * relates the key and values.
+ */
+export interface LabelSelectorRequirement {
+  /**
+   * The label key that the selector applies to.
+   */
+  readonly key: string;
+
+  /**
+   * Represents a key's relationship to a set of values.
+   */
+  readonly operator: LabelSelectorRequirementOperator;
+
+  /**
+   * An array of string values. If the operator is In or NotIn, the values array
+   * must be non-empty. If the operator is Exists or DoesNotExist,
+   * the values array must be empty. This array is replaced during a strategic merge patch.
+   */
+  readonly values?: string[];
 }
 
 /**
@@ -33,40 +84,106 @@ export abstract class Workload extends AbstractPod {
    */
   public readonly podMetadata: ApiObjectMetadataDefinition;
 
-  private readonly _labelSelector: Record<string, string> = {};
+  private readonly _matchLabels: Record<string, string> = {};
+  private readonly _matchExpressions: LabelSelectorRequirement[] = [];
 
   constructor(scope: Construct, id: string, props: WorkloadProps = {}) {
     super(scope, id, props);
 
     this.podMetadata = new ApiObjectMetadataDefinition(props.podMetadata);
 
-    if (props.defaultSelector ?? true) {
+    if (props.select ?? true) {
       const selector = `cdk8s.${this.constructor.name.toLowerCase()}`;
       const matcher = Names.toLabelValue(this);
-      this.podMetadata.addLabel(selector, matcher);
-      this.selectByLabel(selector, matcher);
+      this.select(LabelSelector.is(selector, matcher, true));
     }
 
   }
 
   /**
-   * Configure a label selector to this workload.
-   * Pods that have the label will be selected by workloads configured with this spec.
-   *
-   * @param key - The label key.
-   * @param value - The label value.
+   * Configure selectors for this workload.
    */
-  public selectByLabel(key: string, value: string) {
-    this._labelSelector[key] = value;
+  public select(...selectors: LabelSelector[]) {
+    for (const selector of selectors) {
+      if (selector.operator) {
+        this._matchExpressions.push({ key: selector.key, values: selector.values, operator: selector.operator });
+      } else {
+        const value = selector.values![0];
+        this._matchLabels[selector.key] = value;
+        if (selector.applyToTemplate) {
+          this.podMetadata.addLabel(selector.key, value);
+        }
+      }
+    }
   }
 
   /**
-   * The labels this workload will match against in order to select pods.
+   * The label matchers this workload will use in order to select pods.
    *
-   * Returns a a copy. Use `selectByLabel()` to add labels.
+   * Returns a a copy. Use `select()` to add label matchers.
    */
-  public get labelSelector(): Record<string, string> {
-    return { ...this._labelSelector };
+  public get matchLabels(): Record<string, string> {
+    return { ...this._matchLabels };
   }
 
+  /**
+   * The expression matchers this workload will use in order to select pods.
+   *
+   * Returns a a copy. Use `select()` to add expression matchers.
+   */
+  public get matchExpressions(): LabelSelectorRequirement[] {
+    return [...this._matchExpressions];
+  }
+
+}
+
+/**
+ * A label selector is a label query over a set of resources.
+ *
+ * @see https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/#label-selectors
+ */
+export class LabelSelector {
+
+  /**
+   * Creates a `matchLabels` entry from the key and value.
+   * Use `applyToTemplate` to also add this label to the pod metadata of the workload.
+   */
+  public static is(key: string, value: string, applyToTemplate?: boolean) {
+    return new LabelSelector(applyToTemplate ?? false, key, [value], undefined);
+  }
+
+  /**
+   * Creates a `matchExpressions` "In" entry.
+   */
+  public static in(key: string, values: string[]) {
+    return new LabelSelector(false, key, values, LabelSelectorRequirementOperator.IN);
+  }
+
+  /**
+   * Creates a `matchExpressions` "NotIn" entry.
+   */
+  public static notIn(key: string, values: string[]) {
+    return new LabelSelector(false, key, values, LabelSelectorRequirementOperator.NOT_IN);
+  }
+
+  /**
+   * Creates a `matchExpressions` "Exists" entry.
+   */
+  public static exists(key: string) {
+    return new LabelSelector(false, key, undefined, LabelSelectorRequirementOperator.EXISTS);
+  }
+
+  /**
+   * Creates a `matchExpressions` "DoesNotExist" entry.
+   */
+  public static doesNotExist(key: string) {
+    return new LabelSelector(false, key, undefined, LabelSelectorRequirementOperator.DOES_NOT_EXIST);
+  }
+
+  private constructor(
+    public readonly applyToTemplate: boolean,
+    public readonly key: string,
+    public readonly values?: string[],
+    public readonly operator?: LabelSelectorRequirementOperator) {
+  }
 }
