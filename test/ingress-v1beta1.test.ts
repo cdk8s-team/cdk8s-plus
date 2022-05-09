4de2922c0277df77af37f5be2de4066257d41444
Eli Polonsky <epolon@amazon.com>
2022-05-09 23:34:32 +0300

    feat(ingress): resource backend (#687)
    Resolves https://github.com/cdk8s-team/cdk8s-plus/issues/366
    
    (cherry picked from commit f030ecbf4566e07bb62135bf04536a5a70a448f2)
    Signed-off-by: Eli Polonsky <epolon@amazon.com>
    
    # Conflicts:
    #	src/ingress-v1beta1.ts

--- test/ingress-v1beta1.test.ts
+++ test/ingress-v1beta1.test.ts
@@ -110,6 +110,23 @@ describe('IngressBackend', () => {
       expect(() => IngressV1Beta1Backend.fromService(service, { port: 1234 })).toThrow(/service exposes ports 6011,1111 but backend is defined to use port 1234/);
     });
   });
+
+  test('fromResource', () => {
+
+    const chart = Testing.chart();
+    const service = new Service(chart, 'my-service');
+
+    // THEN
+    expect(IngressV1Beta1Backend.fromResource(service)._toKube()).toEqual({
+      resource: {
+        apiGroup: 'core',
+        kind: 'Service',
+        name: 'test-my-service-c8493104',
+      },
+    });
+
+  });
+
 });
 
 describe('Ingress', () => {
