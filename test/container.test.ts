9dbdc80027e3feaf73c24657f62228c4a4c43100
serhatcetinkaya <serhatcetinkaya@pm.me>
2024-03-01 13:51:36 +0300

    test/container: include capabilities in custom test
    Signed-off-by: serhatcetinkaya <serhatcetinkaya@pm.me>

--- test/container.test.ts
+++ test/container.test.ts
@@ -1,7 +1,7 @@
 import * as cdk8s from 'cdk8s';
 import { Size, Testing } from 'cdk8s';
 import * as kplus from '../src';
-import { Container, Cpu, Handler, ConnectionScheme, Probe, k8s } from '../src';
+import { Container, Cpu, Handler, ConnectionScheme, Probe, k8s, Capability } from '../src';
 
 describe('EnvValue', () => {
 
@@ -707,6 +707,14 @@ test('custom security context', () => {
       privileged: true,
       user: 1000,
       group: 2000,
+      capabilities: {
+        add: [
+          Capability.AUDIT_CONTROL,
+        ],
+        drop: [
+          Capability.BPF,
+        ],
+      },
     },
   });
 
@@ -715,6 +723,8 @@ test('custom security context', () => {
   expect(container.securityContext.readOnlyRootFilesystem).toBeTruthy();
   expect(container.securityContext.user).toEqual(1000);
   expect(container.securityContext.group).toEqual(2000);
+  expect(container.securityContext.capabilities?.add).toEqual(['CAP_AUDIT_CONTROL']);
+  expect(container.securityContext.capabilities?.drop).toEqual(['CAP_BPF']);
 
 });
 
